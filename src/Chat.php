3990e8f85486a39697ac347d71ebbbfc3e342053
Christopher Hertel <mail@christopher-hertel.de>
2024-10-19 21:36:52 +0200

    feat: initial setup of demo app

---
+++ src/Chat.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use PhpLlm\LlmChain\ChainInterface;
+use PhpLlm\LlmChain\Model\Message\Message;
+use PhpLlm\LlmChain\Model\Message\MessageBag;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+final class Chat
+{
+    private const SESSION_KEY = 'rag-chat';
+
+    public function __construct(
+        private readonly RequestStack $requestStack,
+        #[Autowire(service: 'llm_chain.chain.rag')]
+        private readonly ChainInterface $chain,
+    ) {
+    }
+
+    public function loadMessages(): MessageBag
+    {
+        $messages = new MessageBag(
+            Message::forSystem(<<<PROMPT
+                You are an helpful assistant and answer questions about the Web Summer Camp conference.
+                You use the tool 'similarity_search' to search for information that you use for generating the answer.
+                Don't make up information and if you can't find something, just say so.
+                PROMPT
+            )
+        );
+
+        return $this->requestStack->getSession()->get(self::SESSION_KEY, $messages);
+    }
+
+    public function submitMessage(string $message): void
+    {
+        $messages = $this->loadMessages();
+
+        $messages[] = Message::ofUser($message);
+        $response = $this->chain->call($messages);
+        $messages[] = Message::ofAssistant($response->getContent());
+
+        $this->saveMessages($messages);
+    }
+
+    public function reset(): void
+    {
+        $this->requestStack->getSession()->remove(self::SESSION_KEY);
+    }
+
+    private function saveMessages(MessageBag $messages): void
+    {
+        $this->requestStack->getSession()->set(self::SESSION_KEY, $messages);
+    }
+}
---
+++ src/Wikipedia.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use PhpLlm\LlmChain\ChainInterface;
+use PhpLlm\LlmChain\Model\Message\Message;
+use PhpLlm\LlmChain\Model\Message\MessageBag;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+final class Wikipedia
+{
+    private const SESSION_KEY = 'wikipedia-chat';
+
+    public function __construct(
+        private readonly RequestStack $requestStack,
+        #[Autowire(service: 'llm_chain.chain.wikipedia')]
+        private readonly ChainInterface $chain,
+    ) {
+    }
+
+    public function loadMessages(): MessageBag
+    {
+        $default = new MessageBag();
+        $default[] = Message::forSystem('Please answer the users question based on Wikipedia and provide a link to the article.');
+
+        return $this->requestStack->getSession()->get(self::SESSION_KEY, $default);
+    }
+
+    public function submitMessage(string $message): void
+    {
+        $messages = $this->loadMessages();
+
+        $messages[] = Message::ofUser($message);
+        $response = $this->chain->call($messages);
+        $messages[] = Message::ofAssistant($response->getContent());
+
+        $this->saveMessages($messages);
+    }
+
+    public function reset(): void
+    {
+        $this->requestStack->getSession()->remove(self::SESSION_KEY);
+    }
+
+    private function saveMessages(MessageBag $messages): void
+    {
+        $this->requestStack->getSession()->set(self::SESSION_KEY, $messages);
+    }
+}
